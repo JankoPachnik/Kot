1f5dacaea05a86d26249b3113e68c6aa035d1959
Janek Pachnik <januzpachnik@gmailcom>
2019-06-10 14:55:02 +0200

    javascript

--- static/js/game.js
+++ static/js/game.js
@@ -1,26 +1,3 @@
-const map = [
-[0, 0, 0, 2,'k1',2,0, 0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0,'s',0, 2, 0, 2, 0,0,'d1',0,0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[3, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 2, 0, 0, 2, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0,'m',0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-];
-
 const map_generate = function (map_size=20) {
     for(let i = 0; i<map_size; i++){
         map.push([]);
@@ -30,40 +7,35 @@ const map_generate = function (map_size=20) {
     }
 };
 
-const create_object = function(type, style){
-}
+const create_object = function(type, styles = {backgroundColor:"white", width:"0x", margin:"0px", height:"0px", cssFloat:"none"}){
+    const object = document.createElement(type);
+    object.style.backgroundColor = styles["backgroundColor"];
+    object.style.width = styles["width"];
+    object.style.margin = styles["margin"];
+    object.style.cssFloat = styles["cssFloat"];
+    object.style.height = styles["height"];
+    return object
+};
+
+const create_field = function(){
+    const field = create_object("div", {backgroundColor:"black", width:"30px", margin:"0px", height:"30px", cssFloat:"left"})
+    return field
 }
 
 const map_render = function(map) {
     let counter = 0;
-    const game = document.createElement("div");
-    game.style.width = "900px";
-    game.style.margin = "0px";
-    game.style.height = "600px";
-    game.style.margin = "auto";
+    const game = create_object("div", {width:"900px", height:"600px", margin:"auto"});
     game.setAttribute("id", "game");
     document.body.appendChild(game);
     map.forEach(function (element) {
         element.forEach(function (element) {
-            if (element == 0) {
-                const field = document.createElement("div");
-                field.style.backgroundColor = "black";
-                field.style.width = "30px";
-                field.style.margin = "0px";
-                field.style.cssFloat = "left";
-                field.style.height = "30px";
+            const field = create_field();
+            if (element == 0) { // empty field rendering
                 document.getElementById("game").appendChild(field);
                 counter += 1; // counting the number of fields
-            } else if (element == 1) {
-                const field = document.createElement("div");
-                field.style.width = "30px";
-                field.style.margin = "0px";
-                field.style.cssFloat = "left";
-                field.style.height = "30px";
-                const image = document.createElement("img");
-                image.setAttribute('src', 'static/images/box.jpg');
-                image.style.width = "100%";
-                image.style.height = "100%";
+            } else if (element == 1) { //box rendering
+                const image = create_object("img", {width:"100%", height:"100%"});
+                image.setAttribute("src", "/static/images/box.jpg");
                 field.appendChild(image);
                 document.getElementById("game").appendChild(field);
                 counter += 1; // counting the number of fields
@@ -77,7 +49,7 @@ document.body.style.backgroundColor = "lightgray";
 const map = [
 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'b', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -99,7 +71,5 @@ const map = [
 console.log(map);
 map_render(map);
 document.body.style.backgroundColor = "gray";
-map_generate(20);
-console.log(map);
-map_render();
+
 
