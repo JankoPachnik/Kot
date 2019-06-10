bb81213834819ecdfde2d425c91651569fcf96ad
Janek Pachnik <januzpachnik@gmailcom>
2019-06-10 13:24:38 +0200

    map and js

--- static/js/game.js
+++ static/js/game.js
@@ -2,27 +2,25 @@ const map = [];
 const map_generate = function (map_size=20) {
     for(let i = 0; i<map_size; i++){
         map.push([]);
-        for(let j = 0; j<map_size; j++){
-            if (i == 0 || i == 19)
-                map[i].push(1);
-            else
-                map[i].push(0);
+        for(let j = 0; j<map_size * 1.5; j++){
+            map[i].push(0);
         }
     }
 };
 const map_render = function() {
     let counter = 0;
     const game = document.createElement("div");
-    game.style.width = "600px";
+    game.style.width = "900px";
     game.style.margin = "0px";
     game.style.height = "600px";
+    game.style.margin = "auto";
     game.setAttribute("id", "game");
     document.body.appendChild(game);
     map.forEach(function (element) {
         element.forEach(function (element) {
             if (element == 0) {
                 const field = document.createElement("div");
-                field.style.backgroundColor = "lightblue";
+                field.style.backgroundColor = "black";
                 field.style.width = "30px";
                 field.style.margin = "0px";
                 field.style.cssFloat = "left";
@@ -31,7 +29,7 @@ const map_render = function() {
                 counter += 1;
             } else if (element == 1) {
                 const field = document.createElement("div");
-                field.style.backgroundColor = "magenta";
+                field.style.backgroundColor = "grey";
                 field.style.width = "30px";
                 field.style.margin = "0px";
                 field.style.cssFloat = "left";
@@ -43,7 +41,7 @@ const map_render = function() {
     });
     console.log(counter)
 };
-
+document.body.style.backgroundColor = "lightgray";
 map_generate(20);
 console.log(map);
 map_render();
