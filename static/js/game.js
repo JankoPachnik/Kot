13251d90a01c8268fdd9914670cbed4136064db9
Janek Pachnik <januzpachnik@gmailcom>
2019-06-10 12:58:59 +0200

    nvei

--- static/js/game.js
+++ static/js/game.js
@@ -1,15 +1,17 @@
 const map = [];
 const map_generate = function (map_size=20) {
-    for(let i = 0; i<=map_size; i++){
+    for(let i = 0; i<map_size; i++){
         map.push([]);
         for(let j = 0; j<map_size; j++){
-            if (i == 0 || i == 20)
+            if (i == 0 || i == 19)
                 map[i].push(1);
-            map[i].push(0);
+            else
+                map[i].push(0);
         }
     }
 };
 const map_render = function() {
+    let counter = 0;
     const game = document.createElement("div");
     game.style.width = "600px";
     game.style.margin = "0px";
@@ -26,6 +28,7 @@ const map_render = function() {
                 field.style.cssFloat = "left";
                 field.style.height = "30px";
                 document.getElementById("game").appendChild(field);
+                counter += 1;
             } else if (element == 1) {
                 const field = document.createElement("div");
                 field.style.backgroundColor = "magenta";
@@ -34,9 +37,11 @@ const map_render = function() {
                 field.style.cssFloat = "left";
                 field.style.height = "30px";
                 document.getElementById("game").appendChild(field);
+                counter += 1;
             }
         })
     });
+    console.log(counter)
 };
 
 map_generate(20);
